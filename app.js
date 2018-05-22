08515d0a2cd38c0b794a1c267fce6c566ba9db61
wbigger <claudio.capobianco@istruzione.it>
2018-05-22 09:16:12 +0200

    make menu list dynamic

--- app.js
+++ app.js
@@ -1,33 +1,38 @@
 var app = {
-    title:"Hardware Enjoy",
+    title: "Hardware Enjoy",
     events: [],
-    init: function() {
-            $("#title").html(app.title);  
-            app.load();
+    init: function () {
+        $("#title").html(app.title);
+        app.load();
     },
-    load: function(){
+    load: function () {
         $.ajax(
-        {
-            url:"./data.json",
-            method: "get",
-            dataType: "json",
-        }
-    ).done(app.onSuccess).fail(app.onError);
+            {
+                url: "./data.json",
+                method: "get",
+                dataType: "json",
+            }
+        ).done(app.onSuccess).fail(app.onError);
     },
-    onSuccess: function(jsonData) {
+    onSuccess: function (jsonData) {
         console.log(jsonData);
         app.events = jsonData.events;
         app.hardwareList = jsonData.hardwareList;
         app.writelist();
-        
+
     },
-    onError: function(error) {
+    onError: function (error) {
         console.log("ERROR !! Cause is " + error);
     },
-    writelist: function(){
-    
-}
-}
+    writelist: function () {
+        var str = "";
+        for (i = 0; i < app.hardwareList.length; i++) {
+            str += '<img src="immagini/' + app.hardwareList[i].imgUrl + '" >';
+
+        }
+        $("#menu").html(str);
+    }
 
 
+}
 $(document).ready(app.init);
